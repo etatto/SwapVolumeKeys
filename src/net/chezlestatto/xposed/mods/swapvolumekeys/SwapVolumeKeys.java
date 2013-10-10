0b9deac174d361910826d4c603a3f2771681643d
Emmanuel Tatto <manu@chezlestatto.net>
2013-10-10 20:28:56 +0200

    Rewrite the hook entirely, so the long press works now. Closes #1

--- src/net/chezlestatto/xposed/mods/swapvolumekeys/SwapVolumeKeys.java
+++ src/net/chezlestatto/xposed/mods/swapvolumekeys/SwapVolumeKeys.java
@@ -2,80 +2,75 @@ package net.chezlestatto.xposed.mods.swapvolumekeys;
 
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import static de.robv.android.xposed.XposedHelpers.getObjectField;
-
-import android.app.Instrumentation;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.util.DisplayMetrics;
-import android.view.KeyEvent;
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XC_MethodHook.MethodHookParam;
+import de.robv.android.xposed.XSharedPreferences;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
-public class SwapVolumeKeys implements IXposedHookLoadPackage {
+public class SwapVolumeKeys implements IXposedHookZygoteInit, IXposedHookLoadPackage {
 
-	private static final int FLAG_FROM_XPOSED = 0x2000000;
+	public static final String MY_PACKAGE_NAME = SwapVolumeKeys.class.getPackage().getName();
+	private static XSharedPreferences prefs;
+
+	@Override
+	public void initZygote(StartupParam startupParam) {
+		prefs = new XSharedPreferences(MY_PACKAGE_NAME);
+	}
 	
 	@Override
 	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
 
+		prefs.reload();
+		
 		if (lpparam.packageName.equals("android")) {
-			findAndHookMethod("com.android.internal.policy.impl.PhoneWindowManager", lpparam.classLoader, "interceptKeyBeforeQueueing", android.view.KeyEvent.class, Integer.TYPE, Boolean.TYPE, PhoneWindowManager_interceptKeyBeforeQueueing);
+			findAndHookMethod("android.media.AudioService", lpparam.classLoader, "adjustMasterVolume", int.class, int.class, AudioService_adjustMasterVolume);
+			findAndHookMethod("android.media.AudioService", lpparam.classLoader, "adjustSuggestedStreamVolume", int.class, int.class, int.class, AudioService_adjustSuggestedStreamVolume);
 		}
 
 	}
 
-	public final XC_MethodHook PhoneWindowManager_interceptKeyBeforeQueueing = new XC_MethodHook() {
+	public final XC_MethodHook AudioService_adjustMasterVolume = new XC_MethodHook() {
 
-    	@Override
+		@Override
 		protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-
-			if (getScreenOrientation(param.thisObject) == Configuration.ORIENTATION_LANDSCAPE) {
-
-				KeyEvent keyEvent = (KeyEvent) param.args[0];
-				// If already sent by Xposed, do nothing...
-				if (!keyEventIsFromXposed(keyEvent)) {
-
-					int keyCode = keyEvent.getKeyCode(); 
-					switch (keyCode) {
-					
-						case KeyEvent.KEYCODE_VOLUME_DOWN: 
-						case KeyEvent.KEYCODE_VOLUME_UP:
-							sendKeyEvent(keyEvent.getAction(), (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN ? KeyEvent.KEYCODE_VOLUME_UP : KeyEvent.KEYCODE_VOLUME_DOWN), keyEvent.getFlags());
-							param.setResult(0);
-							break;
-					
-					}
-				}
-
-			}
-		
+			hook(param);
 		}
 
-    };
+	};
 
-    private int getScreenOrientation(Object object) {
+	public final XC_MethodHook AudioService_adjustSuggestedStreamVolume = new XC_MethodHook() {
+
+		@Override
+		protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+			hook(param);
+		}
+
+	};
+
+	private void hook(MethodHookParam param) {
+		if ((Integer) param.args[0] != 0) {
+			int orientation = getScreenOrientation(param.thisObject);
+			if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+				param.args[0] = -1 * (Integer) param.args[0];
+			}
+		}
+	}
+
+	private int getScreenOrientation(Object object) {
 		Context context = (Context) getObjectField(object, "mContext");
-    	DisplayMetrics metrics = context.getResources().getDisplayMetrics();
-    	int orientation = Configuration.ORIENTATION_UNDEFINED;
-    	if (metrics.widthPixels <= metrics.heightPixels) {
-    		orientation = Configuration.ORIENTATION_PORTRAIT;
-        } else {
-        	orientation = Configuration.ORIENTATION_LANDSCAPE;
-        }
-    	return orientation;
-    }
-    
-	private final boolean keyEventIsFromXposed(KeyEvent keyEvent) {
-		return (keyEvent.getFlags() & FLAG_FROM_XPOSED) != 0;
-	}    
-    
-    private void sendKeyEvent(int action, int keyCode, int flags) {
-		KeyEvent keyEvent = new KeyEvent(action, keyCode);
-		// Flagged the key event as sent by Xposed
-		keyEvent = KeyEvent.changeFlags(keyEvent, flags | FLAG_FROM_XPOSED);
-		Instrumentation instrumentation = new Instrumentation();
-		instrumentation.sendKeySync(keyEvent);
-    }
-    
+		DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+		int orientation = Configuration.ORIENTATION_UNDEFINED;
+		if (metrics.widthPixels <= metrics.heightPixels) {
+			orientation = Configuration.ORIENTATION_PORTRAIT;
+		} else {
+			orientation = Configuration.ORIENTATION_LANDSCAPE;
+		}
+		return orientation;
+	}
+
 }
